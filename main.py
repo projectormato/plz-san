26b00cf36766799ee5152bf2f7cf0cdfb5eb3ae5
projectormato <k.keita811.anuy@gmail.com>
2021-09-24 19:55:30 +0900

    feat: 古いバージョンをinit

---
+++ main.py
@@ -0,0 +1,66 @@
+from flask import Flask, request, abort
+import os
+import search
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,ImageSendMessage
+)
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi(os.environ.get('TOKEN'))
+handler = WebhookHandler(os.environ.get('SECRET'))
+
+@app.route("/", methods=['GET'])
+def hello():
+    return "hello"
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    input_message = event.message.text
+    first = input_message.split("\n")[0]
+    result = first.split(" ")[1:]
+    if input_message.startswith("plz"): #特定の文字列から始まるなら
+        url = search.one_include_http(" ".join(result))
+        image_message = ImageSendMessage(
+            original_content_url=url,
+            preview_image_url=url
+        )
+        line_bot_api.reply_message(event.reply_token,image_message)
+        #by debug
+        # mage_message = TextSendMessage(text=url)
+        # line_bot_api.reply_message(event.reply_token,mage_message)
+
+    # get url
+    if input_message.startswith("url"):
+        url = search.one_include_http(" ".join(result))
+        text_message = TextSendMessage(text=url)
+        line_bot_api.reply_message(event.reply_token,text_message)
+
+    # そうじゃないならとりあえず何もしない
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=int(os.environ.get("PORT",5000)))
---
+++ search.py
@@ -0,0 +1,70 @@
+import os
+import googleapiclient.discovery
+import random
+import requests
+import itertools
+api_key = os.environ.get("GOOGLE_SEARCH_API_KEY")
+search_engine = os.environ.get("GOOGLE_SEARCH_ENGINE")
+DUMMY_IMAGE = False
+DEBUG = False
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+service = googleapiclient.discovery.build(
+        "customsearch", "v1", developerKey=api_key)
+
+name_parts = list(itertools.chain(
+        range(ord("a"), ord("z") + 1),
+        range(ord("A"), ord("Z") + 1),
+        range(ord("0"), ord("9") + 1),
+        ))
+
+def image_search(query):
+    if DUMMY_IMAGE:
+        return ["http://www.teu.ac.jp/infomation/2014/images/2014CS_gakubucho.jpg"]
+    response = service.cse().list(
+                q=query,
+                cx=search_engine,
+                lr="lang_ja",
+                num=10,
+                start=1,
+                searchType="image"
+            ).execute()
+    items = response["items"]
+    links = [item.get("link") for item in items]
+    if DEBUG:
+        print("--- searched images ---")
+        for i in links:
+            print(i)
+    return links
+
+
+def only_https(url_list):
+    return [url for url in url_list if url.startswith("https")]
+
+
+def one(query):
+    url_list = image_search(query)
+    return random.choice(only_https(url_list))
+
+
+def random_name(length):
+    return "".join(chr(random.choice(name_parts)) for x in range(length))
+
+
+def delete_files(dir_name):
+    for filename in os.listdir(dir_name):
+        if not filename.startswith("."):
+            os.remove(dir_name + filename)
+
+
+# get root like this: request.url_root
+def one_include_http(query):
+    url = random.choice(image_search(query))
+    if url.startswith("https"):
+        return url
+    if DEBUG:
+        print("--- one image ---")
+        print(url)
+    echo = "https://ximagecho.herokuapp.com/echo?url="
+    return echo + url
